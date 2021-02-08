1cc568aca5c8e7c45de4d56464cdfac5ae05f2f9
Pravin Subhash Khaladkar <49605178+amu03@users.noreply.github.com>
2021-02-08 19:11:13 +0530

    created service file

---
+++ services.php
@@ -0,0 +1,52 @@
+<?php?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Virtue Infotek</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+    <div class="bgimg">
+        
+        <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+            <div class="container">
+                <span class="site-logo-img">
+                 <a href="index.php" class="custom-logo-link" rel="home"><img width="115" height="57"       src="https://virtueinfotek.com/wp-content/uploads/2019/09/cropped-12.png" class="custom-logo" alt="Virtue      Infotek" srcset="https://virtueinfotek.com/wp-content/uploads/2019/09/cropped-12.png 1x,        http://virtueinfotek.com/wp-content/uploads/2019/09/12.png 2x" /></a></span>	
+                <button class="navbar-toggler" type="button" data-toggles="collapse" data-target="#collapsenavbar">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse text-center" id="collapsenavbar">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                        <a href="index.php" class="nav-link text-white">Home</a>
+                        </li>
+                    <li class="nav-item">
+                        <a href="about.php" class="nav-link text-white">About us</a>
+                        </li>
+                    <li class="nav-item">
+                        <a href="services.php" class="nav-link text-white">Services</a>
+                        </li>
+                    <li class="nav-item">
+                        <a href="blog.php" class="nav-link text-white text-white">Blog</a>
+                        </li>
+                    <li class="nav-item">
+                        <a href="contact.php" class="nav-link text-white">Contact</a>
+                        </li>
+                    <li class="nav-item">
+                        <button type="button" class="bt"><a href="#" class="nav-link ">Request Quote</a></button>
+                        </li>
+                    
+                    </ul>
+                </div>
+            </div>
+        </nav></div>
+    </body></html>
