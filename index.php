e7fdf66d668c1e6305789cbec5f3f9ed3d1d09cb
Pravin Subhash Khaladkar <49605178+amu03@users.noreply.github.com>
2021-02-08 19:09:18 +0530

    added index file

---
+++ index.php
@@ -0,0 +1,269 @@
+<?php?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Virtue Infotek</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+    <div class="bgimg">
+        
+        <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+            <div class="container">
+                <span class="site-logo-img">
+                 <a href="index.php" class="custom-logo-link" rel="home"><img width="115" height="57"       src="https://virtueinfotek.com/wp-content/uploads/2019/09/cropped-12.png" class="custom-logo" alt="Virtue      Infotek" srcset="https://virtueinfotek.com/wp-content/uploads/2019/09/cropped-12.png 1x,        http://virtueinfotek.com/wp-content/uploads/2019/09/12.png 2x" /></a></span>	
+                <button class="navbar-toggler" type="button" data-toggles="collapse" data-target="#collapsenavbar">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse text-center" id="collapsenavbar">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                        <a href="index.php" class="nav-link text-white">Home</a>
+                        </li>
+                    <li class="nav-item">
+                        <a href="about.php" class="nav-link text-white">About us</a>
+                        </li>
+                    <li class="nav-item">
+                        <a href="services.php" class="nav-link text-white">Services</a>
+                        </li>
+                    <li class="nav-item">
+                        <a href="blog.php" class="nav-link text-white text-white">Blog</a>
+                        </li>
+                    <li class="nav-item">
+                        <a href="contact.php" class="nav-link text-white">Contact</a>
+                        </li>
+                    <li class="nav-item">
+                        <button type="button" class="bt"><a href="#" class="nav-link ">Request Quote</a></button>
+                        </li>
+                    
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container text-center headerset">
+            <h2>VIRTUE INFOTEK</h2>
+            <h1>"I do not fear computers.<br> I fear lack of them."</h1>
+            <button class="btn btn-primary text-white btn-lg">More Info</button>
+            
+        </div>    
+    </div>
+    <section class="container ourservices text-center">
+        <h1>Services</h1>
+        <h2>How we can help you?</h2>
+        <p>“Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution.”</p>
+        
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-10 d-block m-auto">
+                <div class="imgsetting d-block m-auto">
+                    <a href=""><i class="fa fa-code" style="font-size:48px;color:blue"></i></a>
+                    <h2>Design</h2>
+                    <p>We make site user friendly for core audience to acheive the desired objective.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-10 d-block m-auto">
+                <div class="imgsetting d-block m-auto">
+                  <a href=""><i class="fa fa-code" style="font-size:48px;color:blue"></i></a>
+                    <h2>Development</h2>
+                    <p>We have expertise in various technologies as well as domains to provide quality development</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-10 d-block m-auto">
+                <div class="imgsetting d-block m-auto">
+                    <a href=""><i class="fa fa-bullhorn" style="font-size:48px;color:blue"></i></a>
+                    
+                    <h2>Marketing</h2>
+                    <p>We provide unique planning and strategies based on our analysis to reach out the maximum.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-10 d-block m-auto">
+                <div class="imgsetting d-block m-auto">
+                    
+                   <a href=""><i class="fa fa-check" style="font-size:48px;color:blue"></i></a>
+                    <h2>Software Testing</h2>
+                    <p>Quality Assurance is the best way to achieve customer satisfaction. We make the best out of it.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-10 d-block m-auto">
+                <div class="imgsetting d-block m-auto">
+                    <a href=""><i class="fa fa-bullhorn" style="font-size:48px;color:blue"></i></a>
+                    <h2>eCommerce</h2>
+                    <p>We provide unique platform for our Clients to show their products and sell them online.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-10 d-block m-auto">
+                <div class="imgsetting d-block m-auto">
+                
+                    <a href="#"><i class="fa fa-edit" style="font-size:48px;color:blue"></i></a>
+                    <h2>Help & Support</h2>
+                    <p>We perform the best to enhance the knowledge and throw support.</p>
+                </div>
+            </div>      
+        </div>
+        
+    </section>
+    
+    
+    <section class="portfolio bg-light">
+        <div class="container text-center">
+            <h1>PORTFOLIO</h1>
+            <P></P>
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-12 col-10 d-block m-auto">
+                <div class="card">
+                    <img src="images/port1.jpg" style="height:165px" class="card-img img-fluid">
+                    <div class="card-body">
+                        <a href="#"><h4 class="card-title">View Products -></h4></a>
+                       
+                    </div>
+                    </div>
+                </div>
+            <div class="col-lg-3 col-md-3 col-sm-12 col-10 d-block m-auto">
+                <div class="card">
+                    <img src="images/port2.jpg" style="height:165px" class="card-img img-fluid">
+                    <div class="card-body">
+                       <a href="#"><h4 class="card-title">View Products -></h4></a> 
+                        
+                    </div>
+                    </div>
+                </div>
+            <div class="col-lg-3 col-md-3 col-sm-12 col-10 d-block m-auto">
+                <div class="card">
+                    <img src="images/port3.jpg" style="height:165px" class="card-img img-fluid">
+                    <div class="card-body">
+                        <a href="#"><h4 class="card-title">View Products -></h4></a>
+                       
+                    </div>
+                    </div>
+                </div>
+            <div class="col-lg-3 col-md-3 col-sm-12 col-10 d-block m-auto">
+                <div class="card">
+                    <img src="images/port1.jpg" style="height:165px" class="card-img img-fluid">
+                    <div class="card-body">
+                        <a href="#"><h4 class="card-title">View Products -></h4></a>
+                       
+                    </div>
+                    </div>
+                </div>            
+            </div>            
+        </div>    
+    </section>
+    <section class="ourteam">
+        <div class="conatiner text-center">
+        <h1>OUR AMAZING TEAM</h1><br>
+            <div class="row">
+                <div class="col-lg-2 col-md-2 col-sm-10 col-12 d-block m-auto">
+                <figure class="figure">
+                    <img src="images/kalyani.png" class="figure-img img-fluid rounded-circle" height="150px" weight="250px">
+                    <figcaption>
+                    <h4>KALYANI ADHAV</h4>
+                        <p class="figure-caption">CEO</p>
+                    </figcaption>
+                    </figure>
+                </div>
+            <div class="col-lg-2 col-md-2 col-sm-10 col-12 d-block m-auto">
+                <figure class="figure">
+                    <img src="images/piyush.png" class="figure-img img-fluid rounded-circle" height="150px" weight="250px">
+                    <figcaption>
+                    <h4>PIYUSH BISHT</h4>
+                        <p class="figure-caption">DATA ANALYST</p>
+                    </figcaption>
+                    </figure>
+                </div>
+            <div class="col-lg-2 col-md-2 col-sm-10 col-12 d-block m-auto">
+                <figure class="figure">
+                    <img src="images/deepak.png" class="figure-img img-fluid rounded-circle" height="150px" weight="250px">
+                    <figcaption>
+                    <h4>DEEPAK KANOJIA</h4>
+                        <p class="figure-caption">MANAGER</p>
+                    </figcaption>
+                    </figure>
+                </div>
+            <div class="col-lg-2 col-md-2 col-sm-10 col-12 d-block m-auto">
+                <figure class="figure">
+                    <img src="images/karale.png" class="figure-img img-fluid rounded-circle" height="150px" weight="250px">
+                    <figcaption>
+                    <h4>HRISHIKESH KARALE</h4>
+                        <p class="figure-caption">Tech Guy</p>
+                    </figcaption>
+                    </figure>
+                </div>
+            <div class="col-lg-2 col-md-2 col-sm-10 col-12 d-block m-auto">
+                <figure class="figure">
+                    <img src="images/bhupendra.png" class="figure-img img-fluid rounded-circle" height="150px" weight="250px">
+                    <figcaption>
+                    <h4>BHUPENDRA BANOTHE</h4>
+                        <p class="figure-caption">Tech Guy</p>
+                    </figcaption>
+                    </figure>
+                </div>            
+            </div>
+        </div>        
+    </section>
+    <section class="project">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 col-sm-10 d-block m-auto text-center">
+    <h1>Would you like to start a project with us?</h1>
+                        <p>You can contact us regarding your queries</p>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-10 d-block m-auto">
+               
+    <button type="button" class="bt"> <i class="fa fa-phone">+91-7721917721</i></button>
+            </div>
+        </div>    
+    </section>
+    <section class="contact bg-dark text-white">
+        <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-10 d-block m-auto text-center">
+            <img src="images/logo.jpeg">
+            <p>Virtue Infotek is a IT solution company based in Pune .We Provide services to the client and hence we are servive based company.</p>
+            </div>
+                <div class="col-lg-3 col-md-3 col-sm-10 d-block m-auto text-center">
+                    <h5 class="text-primary">Important Liks</h5>
+                    <div class="link">
+                        <a href=""><button class="btn">>Home</button></a><br>
+                    <a href=""><button class="btn">>About Us</button></a><br>
+                    <a href=""><button class="btn">>Services</button></a><br>
+                    <a href=""><button class="btn">>Contact</button></a></div>
+            </div>
+                <div class="col-lg-3 col-md-3 col-sm-10 d-block m-auto text-center">
+            <h5 class="text-primary">Useful Links</h5>
+                    <div class="link">
+                    <a href=""><button class="btn">>Projects</button></a><br>
+                    <a href=""><button class="btn">>Our Team</button></a><br>
+                    <a href=""><button class="btn">>Facts</button></a><br>
+                    <a href=""><button class="btn">>Customers</button></a>
+                    </div>
+            </div>
+                <div class="col-lg-3 col-md-3 col-sm-10 d-block m-auto text-center">
+                    <div class="glink">
+            <h5 class="text-primary">Get In Touch</h5>
+                    
+                        <a href=""><button class="btn"><i class="fa fa-map-marker">&ensp;</i>1/62/B, Padmadarshan Co-Operative Hsg Soc, Above Pda Office, Near Treasure Park pune , MH 411002</button></a><br>
+                    <a href=""><button class="btn"><i class="fa fa-envelope">&ensp;admin@virtueinfotek.com</i></button></a><br>
+                    <a href=""><button class="btn"><i class="fa fa-phone">&ensp;020-24377740</i></button></a><br>
+                    <a href=""><button class="btn"><i class="fa fa-mobile">&ensp;+91-7721917721</i></button></a></div>
+                    </div>
+        
+            </div>
+    </section>
+    
+    
+    
+    <hr>        
+    <footer class="footer bg-secondary m-auto">
+        <h5>Copyright © 2021 Virtue Infotek | Powered by Virtue Infotek</h5>
+    
+    </footer>
+</body>
+</html>
